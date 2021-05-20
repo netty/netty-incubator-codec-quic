fb7ac178a279644c7a01c54e95ebc7fd5375d95b
Norman Maurer <norman_maurer@apple.com>
2021-05-20 08:41:30 +0200

    Update src/main/java/io/netty/incubator/codec/quic/QuicheQuicStreamChannel.java
    Add comment

--- src/main/java/io/netty/incubator/codec/quic/QuicheQuicStreamChannel.java
+++ src/main/java/io/netty/incubator/codec/quic/QuicheQuicStreamChannel.java
@@ -396,6 +396,7 @@ final class QuicheQuicStreamChannel extends DefaultAttributeMap implements QuicS
         assert eventLoop().inEventLoop();
         this.capacity = capacity;
         boolean mayNeedWrite = ((QuicStreamChannelUnsafe) unsafe()).writeQueued();
+        // we need to re-read this.capacity as writeQueued() may update the capacity.
         updateWritabilityIfNeeded(this.capacity > 0);
         return mayNeedWrite;
     }
