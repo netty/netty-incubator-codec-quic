a5da862cd1bb95bb92770a4cb744d80fe021d0be
lostk1ng <lostking1990@gmail.com>
2021-11-09 18:21:12 +0800

    add zero rtt example

---
+++ src/test/java/io/netty/incubator/codec/quic/example/QuicClientZeroRTTExample.java
@@ -0,0 +1,134 @@
+/*
+ * Copyright 2020 The Netty Project
+ *
+ * The Netty Project licenses this file to you under the Apache License,
+ * version 2.0 (the "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at:
+ *
+ *   https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations
+ * under the License.
+ */
+package io.netty.incubator.codec.quic.example;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.TimeUnit;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.Channel;
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.ChannelInputShutdownReadComplete;
+import io.netty.channel.socket.nio.NioDatagramChannel;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+import io.netty.incubator.codec.quic.EarlyDataSendCallback;
+import io.netty.incubator.codec.quic.QuicChannel;
+import io.netty.incubator.codec.quic.QuicClientCodecBuilder;
+import io.netty.incubator.codec.quic.QuicSessionFileHandler;
+import io.netty.incubator.codec.quic.QuicSessionFileProvider;
+import io.netty.incubator.codec.quic.QuicSslContext;
+import io.netty.incubator.codec.quic.QuicSslContextBuilder;
+import io.netty.incubator.codec.quic.QuicStreamChannel;
+import io.netty.incubator.codec.quic.QuicStreamType;
+import io.netty.util.CharsetUtil;
+import io.netty.util.NetUtil;
+
+public final class QuicClientZeroRTTExample {
+
+    private QuicClientZeroRTTExample() { }
+
+    public static void main(String[] args) throws Exception {
+        QuicSslContext context = QuicSslContextBuilder.forClient().trustManager(InsecureTrustManagerFactory.INSTANCE).
+                applicationProtocols("http/0.9").earlyData(true)
+                .sessionHandler(new QuicSessionFileHandler("./session.bin"))
+                .sessionProvider(new QuicSessionFileProvider("./session.bin"))
+                .build();
+        NioEventLoopGroup group = new NioEventLoopGroup(1);
+        try {
+            ChannelHandler codec = new QuicClientCodecBuilder()
+                    .sslContext(context)
+                    .maxIdleTimeout(5000, TimeUnit.MILLISECONDS)
+                    .initialMaxData(10000000)
+                    // As we don't want to support remote initiated streams just setup the limit for local initiated
+                    // streams in this example.
+                    .initialMaxStreamDataBidirectionalLocal(1000000)
+                    .build();
+
+            Bootstrap bs = new Bootstrap();
+            Channel channel = bs.group(group)
+                    .channel(NioDatagramChannel.class)
+                    .handler(codec)
+                    .bind(0).sync().channel();
+
+            QuicChannel quicChannel = QuicChannel.newBootstrap(channel)
+                    .streamHandler(new ChannelInboundHandlerAdapter() {
+                        @Override
+                        public void channelActive(ChannelHandlerContext ctx) {
+                            // As we did not allow any remote initiated streams we will never see this method called.
+                            // That said just let us keep it here to demonstrate that this handle would be called
+                            // for each remote initiated stream.
+                            ctx.close();
+                        }
+                    })
+                    .remoteAddress(new InetSocketAddress(NetUtil.LOCALHOST4, 9999))
+                    .earlyDataSendCallBack(new EarlyDataSendCallback() {
+                        @Override
+                        public void send(QuicChannel quicChannel) {
+                            QuicStreamChannel streamChannel = createStream(quicChannel);
+                            if (streamChannel != null) {
+                                streamChannel.writeAndFlush(
+                                        Unpooled.copiedBuffer("0rtt stream data\r\n", CharsetUtil.US_ASCII));
+                            }
+                        }
+                    })
+                    .connect()
+                    .get();
+
+            QuicStreamChannel streamChannel = createStream(quicChannel);
+            // Write the data and send the FIN. After this its not possible anymore to write any more data.
+            if (streamChannel != null) {
+                streamChannel.writeAndFlush(Unpooled.copiedBuffer("GET /\r\n", CharsetUtil.US_ASCII))
+                        .addListener(QuicStreamChannel.SHUTDOWN_OUTPUT);
+                streamChannel.closeFuture().sync();
+            }
+            quicChannel.closeFuture().sync();
+            channel.close().sync();
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+
+    static QuicStreamChannel createStream(QuicChannel quicChannel) {
+        try {
+            return quicChannel.createStream(QuicStreamType.BIDIRECTIONAL,
+                    new ChannelInboundHandlerAdapter() {
+                        @Override
+                        public void channelRead(ChannelHandlerContext ctx, Object msg) {
+                            ByteBuf byteBuf = (ByteBuf) msg;
+                            System.err.println(byteBuf.toString(CharsetUtil.US_ASCII));
+                            byteBuf.release();
+                        }
+
+                        @Override
+                        public void userEventTriggered(ChannelHandlerContext ctx, Object evt) {
+                            if (evt == ChannelInputShutdownReadComplete.INSTANCE) {
+                                // Close the connection once the remote peer did send the FIN for this stream.
+                                ((QuicChannel) ctx.channel().parent()).close(true, 0,
+                                        ctx.alloc().directBuffer(16)
+                                                .writeBytes(new byte[]{'k', 't', 'h', 'x', 'b', 'y', 'e'}));
+                            }
+                        }
+                    }).sync().getNow();
+        } catch (Exception e) {
+            return null;
+        }
+    }
+}
