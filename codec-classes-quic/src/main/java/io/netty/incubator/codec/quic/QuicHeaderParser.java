netty/netty

commit 885a18a0ba6dc735791bf8e12d12d643396bbf05

Correctly calculate the memory address even if ByteBuf might not have… (#512)
… a memory adress that is directly accessible.

Motivation:

When we try to obtain a memory address of a ByteBuf we might need to
fallback to obain it via the ByteBuffer. When doing so we also need to
take the position of the ByteBuffer into account as it might be
non-zero.

Modifications:

- Add new static helper methods to Quiche class that can be used to
obtain the reader / writer memory address in a consistent / easy manner.
- Use these new methods

Result:

Always use the correct memory address and so don't end up with
data-corruption

codec-classes-quic/src/main/java/io/netty/incubator/codec/quic/QuicHeaderParser.java
@@ -98,21 +98,25 @@ public final class QuicHeaderParser implements AutoCloseable {
     public void parse(InetSocketAddress sender,
                       InetSocketAddress recipient, ByteBuf packet, QuicHeaderProcessor callback) throws Exception {
         if (closed) {
-            throw new IllegalStateException("QuicHeaderParser is already closed");
+            throw new IllegalStateException(QuicHeaderParser.class.getSimpleName() + " is already closed");
         }
-        long contentAddress = Quiche.memoryAddress(packet) + packet.readerIndex();
-        int contentReadable = packet.readableBytes();
 
-        // Ret various len values so quiche_header_info can make use of these.
+        // Set various len values so quiche_header_info can make use of these.
         scidLenBuffer.setInt(0, Quiche.QUICHE_MAX_CONN_ID_LEN);
         dcidLenBuffer.setInt(0, Quiche.QUICHE_MAX_CONN_ID_LEN);
         tokenLenBuffer.setInt(0, maxTokenLength);
 
-        int res = Quiche.quiche_header_info(contentAddress, contentReadable, localConnectionIdLength,
-                Quiche.memoryAddress(versionBuffer), Quiche.memoryAddress(typeBuffer),
-                Quiche.memoryAddress(scidBuffer), Quiche.memoryAddress(scidLenBuffer),
-                Quiche.memoryAddress(dcidBuffer), Quiche.memoryAddress(dcidLenBuffer),
-                Quiche.memoryAddress(tokenBuffer), Quiche.memoryAddress(tokenLenBuffer));
+        int res = Quiche.quiche_header_info(
+                Quiche.readerMemoryAddress(packet), packet.readableBytes(),
+                localConnectionIdLength,
+                Quiche.memoryAddress(versionBuffer, 0, versionBuffer.capacity()),
+                Quiche.memoryAddress(typeBuffer, 0, versionBuffer.capacity()),
+                Quiche.memoryAddress(scidBuffer, 0, scidBuffer.capacity()),
+                Quiche.memoryAddress(scidLenBuffer, 0, scidLenBuffer.capacity()),
+                Quiche.memoryAddress(dcidBuffer, 0, dcidBuffer.capacity()),
+                Quiche.memoryAddress(dcidLenBuffer, 0, dcidLenBuffer.capacity()),
+                Quiche.memoryAddress(tokenBuffer, 0, tokenBuffer.capacity()),
+                Quiche.writerMemoryAddress(tokenLenBuffer));
         if (res >= 0) {
             int version = versionBuffer.getInt(0);
             byte type = typeBuffer.getByte(0);

codec-classes-quic/src/main/java/io/netty/incubator/codec/quic/Quiche.java
@@ -670,24 +670,42 @@ final class Quiche {
     static native int sockaddr_cmp(long addr, long addr2);
 
     /**
-     * Returns the memory address if the {@link ByteBuf}
+     * Returns the memory address if the {@link ByteBuf} taking the readerIndex into account.
+     *
+     * @param buf   the {@link ByteBuf} of which we want to obtain the memory address
+     *              (taking its {@link ByteBuf#readerIndex()} into account).
+     * @return      the memory address of this {@link ByteBuf}s readerIndex.
      */
-    static long memoryAddress(ByteBuf buf) {
-        assert buf.isDirect();
-        return buf.hasMemoryAddress() ? buf.memoryAddress() :
-                buffer_memory_address(buf.internalNioBuffer(buf.readerIndex(), buf.readableBytes()));
+    static long readerMemoryAddress(ByteBuf buf) {
+        return memoryAddress(buf, buf.readerIndex(), buf.readableBytes());
     }
 
     /**
-     * Returns the memory address of the given {@link ByteBuffer}. If you want to also respect the
-     * {@link ByteBuffer#position()} use {@link #memoryAddressWithPosition(ByteBuffer)}.
+     * Returns the memory address if the {@link ByteBuf} taking the writerIndex into account.
      *
-     * @param buf   the {@link ByteBuffer} of which we want to obtain the memory address..
-     * @return      the memory address of this {@link ByteBuffer}.
+     * @param buf   the {@link ByteBuf} of which we want to obtain the memory address
+     *              (taking its {@link ByteBuf#writerIndex()} into account).
+     * @return      the memory address of this {@link ByteBuf}s writerIndex.
      */
-    static long memoryAddress(ByteBuffer buf) {
+    static long writerMemoryAddress(ByteBuf buf) {
+        return memoryAddress(buf, buf.writerIndex(), buf.writableBytes());
+    }
+
+    /**
+     * Returns the memory address if the {@link ByteBuf} taking the offset into account.
+     *
+     * @param buf       the {@link ByteBuf} of which we want to obtain the memory address
+     *                  (taking the {@code offset} into account).
+     * @param offset    the offset of the memory address.
+     * @param len       the length of the {@link ByteBuf}.
+     * @return          the memory address of this {@link ByteBuf}s offset.
+     */
+    static long memoryAddress(ByteBuf buf, int offset, int len) {
         assert buf.isDirect();
-        return buffer_memory_address(buf);
+        if (buf.hasMemoryAddress()) {
+            return buf.memoryAddress() + offset;
+        }
+        return memoryAddressWithPosition(buf.internalNioBuffer(offset, len));
     }
 
     /**
@@ -699,7 +717,8 @@ final class Quiche {
      * @return      the memory address of this {@link ByteBuffer}s position.
      */
     static long memoryAddressWithPosition(ByteBuffer buf) {
-        return memoryAddress(buf) + buf.position();
+        assert buf.isDirect();
+        return buffer_memory_address(buf) + buf.position();
     }
 
     @SuppressWarnings("deprecation")

codec-classes-quic/src/main/java/io/netty/incubator/codec/quic/QuicheQuicChannel.java
@@ -359,7 +359,7 @@ final class QuicheQuicChannel extends AbstractChannel implements QuicChannel {
             int fromSockaddrLen = SockaddrIn.setAddress(fromSockaddrMemory, local);
             int toSockaddrLen = SockaddrIn.setAddress(toSockaddrMemory, remote);
             QuicheQuicConnection connection = quicheEngine.createConnection(ssl ->
-                    Quiche.quiche_conn_new_with_tls(Quiche.memoryAddress(idBuffer) + idBuffer.readerIndex(),
+                    Quiche.quiche_conn_new_with_tls(Quiche.readerMemoryAddress(idBuffer),
                             idBuffer.readableBytes(), -1, -1,
                             Quiche.memoryAddressWithPosition(fromSockaddrMemory), fromSockaddrLen,
                             Quiche.memoryAddressWithPosition(toSockaddrMemory), toSockaddrLen,
@@ -590,7 +590,7 @@ final class QuicheQuicChannel extends AbstractChannel implements QuicChannel {
 
         failPendingConnectPromise();
         Quiche.throwIfError(Quiche.quiche_conn_close(connectionAddressChecked(), app, err,
-                Quiche.memoryAddress(reason) + reason.readerIndex(), reason.readableBytes()));
+                Quiche.readerMemoryAddress(reason), reason.readableBytes()));
 
         // As we called quiche_conn_close(...) we need to ensure we will call quiche_conn_send(...) either
         // now or we will do so once we see the channelReadComplete event.
@@ -695,7 +695,7 @@ final class QuicheQuicChannel extends AbstractChannel implements QuicChannel {
 
     private int sendDatagram(ByteBuf buf) throws ClosedChannelException {
         return Quiche.quiche_conn_dgram_send(connectionAddressChecked(),
-                Quiche.memoryAddress(buf) + buf.readerIndex(), buf.readableBytes());
+                Quiche.readerMemoryAddress(buf), buf.readableBytes());
     }
 
     @Override
@@ -854,7 +854,7 @@ final class QuicheQuicChannel extends AbstractChannel implements QuicChannel {
 
     private int streamSend0(long streamId, ByteBuf buffer, boolean fin) throws ClosedChannelException {
         return Quiche.quiche_conn_stream_send(connectionAddressChecked(), streamId,
-                Quiche.memoryAddress(buffer) + buffer.readerIndex(), buffer.readableBytes(), fin);
+                Quiche.readerMemoryAddress(buffer), buffer.readableBytes(), fin);
     }
 
     private int streamSend(long streamId, ByteBuffer buffer, boolean fin) throws ClosedChannelException {
@@ -867,14 +867,13 @@ final class QuicheQuicChannel extends AbstractChannel implements QuicChannel {
             finBuffer = alloc().directBuffer(1);
         }
         int writerIndex = buffer.writerIndex();
-        long memoryAddress = Quiche.memoryAddress(buffer);
         int recvLen = Quiche.quiche_conn_stream_recv(connectionAddressChecked(), streamId,
-                memoryAddress + writerIndex, buffer.writableBytes(), Quiche.memoryAddress(finBuffer));
+                Quiche.writerMemoryAddress(buffer), buffer.writableBytes(), Quiche.writerMemoryAddress(finBuffer));
         if (Quiche.throwIfError(recvLen)) {
             return StreamRecvResult.DONE;
-        } else {
-            buffer.writerIndex(writerIndex + recvLen);
         }
+
+        buffer.writerIndex(writerIndex + recvLen);
         return finBuffer.getBoolean(0) ? StreamRecvResult.FIN : StreamRecvResult.OK;
     }
 
@@ -1052,7 +1051,7 @@ final class QuicheQuicChannel extends AbstractChannel implements QuicChannel {
                 boolean done;
                 int writerIndex = out.writerIndex();
                 int written = Quiche.quiche_conn_send(
-                        connAddr, Quiche.memoryAddress(out) + writerIndex, out.writableBytes(),
+                        connAddr, Quiche.writerMemoryAddress(out), out.writableBytes(),
                         Quiche.memoryAddressWithPosition(sendInfo));
                 if (written == 0) {
                     out.release();
@@ -1175,7 +1174,7 @@ final class QuicheQuicChannel extends AbstractChannel implements QuicChannel {
             int writerIndex = out.writerIndex();
 
             int written = Quiche.quiche_conn_send(
-                    connAddr, Quiche.memoryAddress(out) + writerIndex, out.writableBytes(),
+                    connAddr, Quiche.writerMemoryAddress(out), out.writableBytes(),
                     Quiche.memoryAddressWithPosition(sendInfo));
 
             try {
@@ -1406,8 +1405,7 @@ final class QuicheQuicChannel extends AbstractChannel implements QuicChannel {
                     tmpBuffer.writeBytes(buffer);
                     buffer = tmpBuffer;
                 }
-                int bufferReaderIndex = buffer.readerIndex();
-                long memoryAddress = Quiche.memoryAddress(buffer) + bufferReaderIndex;
+                long memoryAddress = Quiche.readerMemoryAddress(buffer);
 
                 ByteBuffer recvInfo = connection.nextRecvInfo();
                 QuicheRecvInfo.setRecvInfo(recvInfo, sender, recipient);
@@ -1463,7 +1461,7 @@ final class QuicheQuicChannel extends AbstractChannel implements QuicChannel {
                         bufferReadable -= res;
                     } while (bufferReadable > 0);
                 } finally {
-                    buffer.skipBytes((int) (memoryAddress - Quiche.memoryAddress(buffer)));
+                    buffer.skipBytes((int) (memoryAddress - Quiche.readerMemoryAddress(buffer)));
                     if (tmpBuffer != null) {
                         tmpBuffer.release();
                     }

codec-classes-quic/src/main/java/io/netty/incubator/codec/quic/QuicheQuicServerCodec.java
@@ -123,9 +123,9 @@ final class QuicheQuicServerCodec extends QuicheQuicCodec {
             int outWriterIndex = out.writerIndex();
 
             int res = Quiche.quiche_negotiate_version(
-                    Quiche.memoryAddress(scid) + scid.readerIndex(), scid.readableBytes(),
-                    Quiche.memoryAddress(dcid) + dcid.readerIndex(), dcid.readableBytes(),
-                    Quiche.memoryAddress(out) + outWriterIndex, out.writableBytes());
+                    Quiche.readerMemoryAddress(scid), scid.readableBytes(),
+                    Quiche.readerMemoryAddress(dcid), dcid.readableBytes(),
+                    Quiche.writerMemoryAddress(out), out.writableBytes());
             if (res < 0) {
                 out.release();
                 Quiche.throwIfError(res);
@@ -150,12 +150,13 @@ final class QuicheQuicServerCodec extends QuicheQuicCodec {
 
                 ByteBuf out = ctx.alloc().directBuffer(Quic.MAX_DATAGRAM_SIZE);
                 int outWriterIndex = out.writerIndex();
-                int written = Quiche.quiche_retry(Quiche.memoryAddress(scid) + scid.readerIndex(), scid.readableBytes(),
-                        Quiche.memoryAddress(dcid) + dcid.readerIndex(), dcid.readableBytes(),
-                        Quiche.memoryAddress(connIdBuffer) + connIdBuffer.readerIndex(), connIdBuffer.readableBytes(),
-                        Quiche.memoryAddress(mintTokenBuffer) + mintTokenBuffer.readerIndex(),
-                        mintTokenBuffer.readableBytes(),
-                        version, Quiche.memoryAddress(out) + outWriterIndex, out.writableBytes());
+                int written = Quiche.quiche_retry(
+                        Quiche.readerMemoryAddress(scid), scid.readableBytes(),
+                        Quiche.readerMemoryAddress(dcid), dcid.readableBytes(),
+                        Quiche.readerMemoryAddress(connIdBuffer), connIdBuffer.readableBytes(),
+                        Quiche.readerMemoryAddress(mintTokenBuffer), mintTokenBuffer.readableBytes(),
+                        version,
+                        Quiche.writerMemoryAddress(out), out.writableBytes());
 
                 if (written < 0) {
                     out.release();
@@ -188,7 +189,7 @@ final class QuicheQuicServerCodec extends QuicheQuicCodec {
             key = connectionIdAddressGenerator.newId(
                     dcid.internalNioBuffer(dcid.readerIndex(), dcid.readableBytes()), localConnIdLength);
             connIdBuffer.writeBytes(key.duplicate());
-            scidAddr = Quiche.memoryAddress(connIdBuffer) + connIdBuffer.readerIndex();
+            scidAddr = Quiche.readerMemoryAddress(connIdBuffer);
             scidLen = localConnIdLength;
             ocidAddr = -1;
             ocidLen = -1;
@@ -198,9 +199,9 @@ final class QuicheQuicServerCodec extends QuicheQuicCodec {
                 return existingChannel;
             }
         } else {
-            scidAddr = Quiche.memoryAddress(dcid) + dcid.readerIndex();
+            scidAddr = Quiche.readerMemoryAddress(dcid);
             scidLen = localConnIdLength;
-            ocidAddr = Quiche.memoryAddress(token) + offset;
+            ocidAddr = Quiche.memoryAddress(token, offset, token.readableBytes());
             ocidLen = token.readableBytes() - offset;
             // Now create the key to store the channel in the map.
             byte[] bytes = new byte[localConnIdLength];

netty/netty-incubator-codec-quic

commit 655af61555607de2aeb8395d0bd68edf450cba13

Fixed Android connection not working
Motivation:

Since version 0.0.42.Final the connection from an android client was not working anymore, caused by a QuicClosedChannelException. After inspecting the changes I found that 885a18a0ba6dc735791bf8e12d12d643396bbf05 introduced the bug.

Modifications:

The call to Quiche.memoryAddress used the capacity of the wrong buffer.
The bug only appeared on Android because on most other platforms the buf.hasMemoryAddress() is true, and the len parameter is not used therefore.

Result:

Make netty-incubator-codec-quic work on Android again

codec-classes-quic/src/main/java/io/netty/incubator/codec/quic/QuicHeaderParser.java
@@ -110,7 +110,7 @@ public final class QuicHeaderParser implements AutoCloseable {
                 Quiche.readerMemoryAddress(packet), packet.readableBytes(),
                 localConnectionIdLength,
                 Quiche.memoryAddress(versionBuffer, 0, versionBuffer.capacity()),
-                Quiche.memoryAddress(typeBuffer, 0, versionBuffer.capacity()),
+                Quiche.memoryAddress(typeBuffer, 0, typeBuffer.capacity()),
                 Quiche.memoryAddress(scidBuffer, 0, scidBuffer.capacity()),
                 Quiche.memoryAddress(scidLenBuffer, 0, scidLenBuffer.capacity()),
                 Quiche.memoryAddress(dcidBuffer, 0, dcidBuffer.capacity()),